7d7c038650a2934e6def9a7805bdd2a4639643de
NESTOR RICARDO GONZALEZ CASADOS <ing.nestorricardo@gmail.com>
2022-02-11 10:29:11 -0600

    Generar QR

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,22 @@
-function doClick(e) {
+/*function doClick(e) {
 	alert($.label.text);
-}
+}*/
+
+// require qrcode and spesify text and errorCorrectLevel
+var qrcode = require('qrcode').QRCode({
+	text: 'https://www.facebook.com/Ricardo.dagak',		// text string for QR code
+	errorCorrectLevel: 'M'					// Possible Values: L / M / Q / H
+});
+
+
+// create Ti.UI.ImageView qrcodeView - spesify width, height and margin
+var qrcodeView = qrcode.createQRCodeView({
+	width: 300,
+	height: 300,
+	margin: 4,
+});
+
+// add qrcodeView to youViewNameHere
+$.qr.add(qrcodeView);
 
 $.index.open();
